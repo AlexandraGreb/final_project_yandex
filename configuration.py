307183c477ff828064310dd1569cef9a4cbee9a3
Александра Гребенщикова <aleksandragrebensikova@MacBook-Air-Aleksandra-2.local>
2025-02-17 00:24:49 +0300

    new info

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://28bd125f-477a-4dc4-9b4a-aa01d2996353.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://ed336645-1f50-4b85-b479-70b7ecdca6cd.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track"
